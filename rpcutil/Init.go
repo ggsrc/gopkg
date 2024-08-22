0969e219efd1b5db8a101a580b8ec488a8ae2a48
pengdaqian <pengdaqian@galxe.com>
2024-08-22 14:51:27 +0800

    Revert "add grpc interceptors"
    This reverts commit 6f664970114bc3fb1e834fb7ad29e861f23b9297.

--- rpcutil/Init.go
+++ rpcutil/Init.go
@@ -36,10 +36,9 @@ type RpcInitHelperOptions struct {
 	InitProfiling bool
 	ProfilingConf *profiling.Config
 
-	InitGrpcServer   bool
-	GrpcServerConf   *gg_grpc.ServerConfig
-	GrpcInterceptors []grpc.UnaryServerInterceptor
-	GrpcServerOpt    []grpc.ServerOption
+	InitGrpcServer bool
+	GrpcServerConf *gg_grpc.ServerConfig
+	GrpcServerOpt  []grpc.ServerOption
 
 	InitCronJob bool
 	CronJobOpt  []gocron.SchedulerOption
