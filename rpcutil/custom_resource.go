cef07118e32e41a2ce2b3de90a3a2743fd5c5663
pengdaqian <pengdaqian@galxe.com>
2025-03-07 15:34:15 +0800

    feat custom resource

--- rpcutil/custom_resource.go
+++ rpcutil/custom_resource.go
@@ -6,3 +6,23 @@ type CustomResource interface {
 	Start(ctx context.Context) error
 	Close(ctx context.Context) error
 }
+
+func WrapCustomResource(startFunc, closeFunc func(ctx context.Context) error) CustomResource {
+	return &wrappedResource{
+		start: startFunc,
+		close: closeFunc,
+	}
+}
+
+type wrappedResource struct {
+	start func(ctx context.Context) error
+	close func(ctx context.Context) error
+}
+
+func (r *wrappedResource) Start(ctx context.Context) error {
+	return r.start(ctx)
+}
+
+func (r *wrappedResource) Close(ctx context.Context) error {
+	return r.close(ctx)
+}
