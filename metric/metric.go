11ef19f1945111264d9bdd3ff1d1405543fe695d
xmujx <117618147+xmujx@users.noreply.github.com>
2025-07-07 10:48:07 +0800

    feat: add RecordEvent

--- metric/metric.go
+++ metric/metric.go
@@ -2,12 +2,21 @@ package metric
 
 import (
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
 	"net/http"
 	"time"
 
 	"github.com/kelseyhightower/envconfig"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var counters = map[string]*prometheus.CounterVec{}
+
+type MetricEvent struct {
+	Name   string
+	Labels map[string]string
+	Value  float64
+}
 
 type Server struct {
 	conf *Config
@@ -34,4 +43,27 @@ func (s *Server) Start() error {
 		ReadHeaderTimeout: time.Second * 5,
 	}
 	return server.ListenAndServe()
+}
+
+// RecordEvent auto register counter
+func RecordEvent(e MetricEvent) {
+	labelKeys := []string{}
+	labelValues := []string{}
+	for k, v := range e.Labels {
+		labelKeys = append(labelKeys, k)
+		labelValues = append(labelValues, v)
+	}
+
+	// 缓存 & 注册
+	counter, ok := counters[e.Name]
+	if !ok {
+		counter = prometheus.NewCounterVec(
+			prometheus.CounterOpts{Name: e.Name, Help: "auto generated"},
+			labelKeys,
+		)
+		prometheus.MustRegister(counter)
+		counters[e.Name] = counter
+	}
+
+	counter.WithLabelValues(labelValues...).Add(e.Value)
 }
